ab5845b5370778d777f71e4e284e0ad84f30f541
sezaite <martyna.sezaite@gmail.com>
2021-07-16 01:07:38 +0300

    store list

--- src/components/main-page/StoreList.js
+++ src/components/main-page/StoreList.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import ListItem from './ListItem';
 
-function StoreList() {
+function StoreList({ data }) {
     return (
-        <div>
-
-        </div>
+        <div className="articles-container">
+            <div className="row">
+                {data.map((item) => (
+                    <ListItem key={irtictem.id} price={item.price} title={item.title} description={item.description} img={item.image} />
+                ))}
+            </div>
+        </div >
     )
 }
 
