08f9f26877c98d64cc2a82f525771b63d6940aa5
sezaite <martyna.sezaite@gmail.com>
2021-07-15 16:36:06 +0300

    basic folder structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import './App.scss';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
+
+  <Router>
+    <Switch>
+      <Route exact path="/" component={ } />
+      <Route exact path="/product/:id" component={ } />
+      <Route exact path="/cart" component={ } />
+    </Switch>
+  </Router>
 
 }
 
