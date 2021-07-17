3e144d6268691819f06e68e4139e0a01e15ff590
sezaite <martyna.sezaite@gmail.com>
2021-07-17 23:38:40 +0300

    adds to the cart

--- src/components/cart/CartItem.js
+++ src/components/cart/CartItem.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { addToCart, removeFromCart } from '../../state/actions/cartActions';
 
 function CartItem({ id, price, title, description, img, amount }) {
+    const dispatch = useDispatch();
     return (
         <div className="cart-block">
             <img src={img} alt="" />
             <h4>{title}</h4>
             <h6>{price}</h6>
             <h6>{amount}</h6>
+            <div className="amount">
+                <button className="dec-btn" onClick={() => dispatch(removeFromCart(id))}>-</button>
+                <button className="inc-btn" onClick={() => dispatch(addToCart(id))}>+</button>
+            </div>
         </div>
     )
 }
