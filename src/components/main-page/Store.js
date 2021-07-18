920cbec64dabe55d022c3e7ac3701bc897f0178d
sezaite <martyna.sezaite@gmail.com>
2021-07-18 17:24:43 +0300

    half responsive

--- src/components/main-page/Store.js
+++ src/components/main-page/Store.js
@@ -12,21 +12,30 @@ function Store() {
     const dispatch = useDispatch();
 
     useEffect(() => {
-        // data === null ? console.log('Fetching...') : dispatch(setStore(data));
         if (data !== null) dispatch(setStore(data));
     }, [data]);
 
     return (
         <div className="store-page">
-            <label htmlFor="sort">Sort by price:</label>
-            <select defaultValue="select" name="sort" id="cars" onChange={(e) => setSort(e.target.value)}>
-                <option disabled value="select">Select</option>
-                <option value="asc">Ascending</option>
-                <option value="desc">Descending</option>
-            </select>
+
             <CartMenu />
             {error && <h2 className="error-msg">Could not load products</h2>}
-            {isFetching ? <h2 className="loading-msg">Loading...</h2> : <StoreList items={data} sort={sort} />}
+            {isFetching ? <h2 className="loading-msg">Loading...</h2> :
+                <>
+                    <div className="page-top">
+                        <div className="sort">
+                            <label htmlFor="sort">Sort by price:</label>
+                            <select defaultValue="select" name="sort" id="cars" onChange={(e) => setSort(e.target.value)}>
+                                <option disabled value="select">Select</option>
+                                <option value="asc">Ascending</option>
+                                <option value="desc">Descending</option>
+                            </select>
+                        </div>
+                        <h6>{data.length} products found</h6>
+                    </div>
+                    <StoreList items={data} sort={sort} />
+                </>
+            }
 
         </div>
     )
--- src/components/main-page/StoreList.js
+++ src/components/main-page/StoreList.js
@@ -6,11 +6,9 @@ function StoreList({ items, sort }) {
 
     return (
         <div className="items-container">
-            <div className="row">
-                {items.map((item) => (
-                    <ListItem key={item.id} id={item.id} price={item.price} title={item.title} description={item.description} img={item.image} />
-                ))}
-            </div>
+            {items.map((item) => (
+                <ListItem key={item.id} id={item.id} price={item.price} title={item.title} description={item.description} img={item.image} />
+            ))}
         </div >
     )
 }
