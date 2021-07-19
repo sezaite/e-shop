40fc8b12991c542acba075524131e3dc9584264c
sezaite <martyna.sezaite@gmail.com>
2021-07-19 09:11:41 +0300

    cart styling

--- src/components/cart/CartItem.js
+++ src/components/cart/CartItem.js
@@ -8,17 +8,19 @@ function CartItem({ id, price, title, description, img, amount }) {
     const dispatch = useDispatch();
     return (
         <div className="cart-block">
-            <button className="remove-btn" onClick={() => dispatch(removeFromCart(id))}><FontAwesomeIcon icon={faTimes} /></button>
-            <LinesEllipsis
-                text={title}
-                maxLine='1'
-                ellipsis='...'
-                trimRight
-                basedOn='words'
-                className='cart-elipsis'
-            />
-            <h6 className="price">{price}</h6>
-            <h6 className="amount">{amount}</h6>
+            <button className="remove-btn" onClick={() => dispatch(removeFromCart(id))}><FontAwesomeIcon icon={faTimes} className="x-icon" /></button>
+            <div className="cart-block-top">
+                <LinesEllipsis
+                    text={title}
+                    maxLine='1'
+                    ellipsis='...'
+                    trimRight
+                    basedOn='words'
+                    className='cart-elipsis'
+                />
+                <h6 className="price">{price}</h6>
+            </div>
+            <h6 className="amount">Quantity: {amount}</h6>
             <div className="amount-control">
                 <button className="inc-btn" onClick={() => dispatch(decrement(id))}>-</button>
                 <button className="desc-btn" onClick={() => dispatch(addToCart(id))}>+</button>
