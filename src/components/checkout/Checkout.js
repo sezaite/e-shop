a3e16bc668417467e07f409d168982423849ef06
sezaite <martyna.sezaite@gmail.com>
2021-07-19 23:44:31 +0300

    bugs and errors fixed

--- src/components/checkout/Checkout.js
+++ src/components/checkout/Checkout.js
@@ -1,21 +1,32 @@
 import CartList from '../cart/CartList';
 import { emptyCart } from '../../state/actions/cartActions';
 import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from "react-router-dom";
 
 function Checkout() {
     const cart = useSelector((state) => state.cartReducer.cart);
     const dispatch = useDispatch();
-
+    let history = useHistory();
 
     return (
-        cart.length > 0 ? (
-            <div className="cart-page">
-                {<CartList cart={cart} />}
-                <button className="inc-btn" onClick={() => dispatch(emptyCart())}>Empty cart</button>
-            </div>)
-            :
-            <h2 className="empty-cart">Your cart is empty</h2>
 
+        <div className="cart-page">
+            {cart.length > 0 ? (
+                <div className="cart">
+                    {<CartList cart={cart} />}
+                    <button className="empty-btn btn" onClick={() => dispatch(emptyCart())}>Empty cart</button>
+                    <button className="btn back" onClick={() => history.goBack()}>Back</button>
+                    <button className="btn payment">Proceed to payment</button>
+
+                </div>)
+                :
+                <div className="empty-checkout">
+                    <h2 className="empty-cart">Your cart is empty</h2>
+                    <button className="btn back" onClick={() => history.goBack()}>Back</button>
+                </div>
+            }
+
+        </div>
 
     )
 }
