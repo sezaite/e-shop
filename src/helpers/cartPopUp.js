f08959e0b8b5de936f82120745d5941c67b89332
sezaite <martyna.sezaite@gmail.com>
2021-07-20 14:03:01 +0300

    path cathced

--- src/helpers/cartPopUp.js
+++ src/helpers/cartPopUp.js
@@ -2,9 +2,8 @@ function cartPopUp() {
     const cartMenu = document.querySelector('.cart-menu');
     cartMenu.classList.remove('hidden');
     window.addEventListener('click', (e) => {
-        console.log(e.target);
         const all = cartMenu.querySelectorAll("*");
-        if (e.target.classList.contains("add-to-cart") || e.target.classList.contains("x-icon")) return;
+        if (e.target.classList.contains("add-to-cart") || e.target === cartMenu || e.target.classList.contains("remove-btn") || e.target.nodeName === "path") return;
         for (let one of all) {
             if (one === e.target) return;
         }
